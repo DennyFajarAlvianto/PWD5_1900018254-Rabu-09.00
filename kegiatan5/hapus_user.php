350ff88ac71f41f0050d183e9ac7a15a6fda626a
DennyFajarAlvianto <dennyfajar257@gmail.com>
2021-11-24 21:45:17 +0700

    commit on 'master'

--- kegiatan5/hapus_user.php
+++ kegiatan5/hapus_user.php
@@ -1,7 +1,11 @@
 <?php
+//memasukkan file kedalam dokumen kita, file tersebut bisa script PHP, file HTML dll
     include "koneksi.php";
+//variabel sql untuk menghapus data menggunakan pencarian id
     $sql="delete from users where id_user= '$_GET[id]'";
+    //variabel untuk mengkoneksi ke database
     mysqli_query($con, $sql);
+    //variabel untuk menutup database
     mysqli_close($con);
     header('location:tampil_user.php');
 ?>
--- tampil_user.php
+++ tampil_user.php
@@ -5,17 +5,12 @@
     </form>
     <table>
         <tr><th>No</th><th>Username</th><th>NamaLengkap</th><th>Email</th><th>Aksi</th></tr>";
-        //memasukkan file kedalam dokumen kita, file tersebut bisa script PHP, file HTML dll
         include "koneksi.php";
-        //sql untuk menampilkan data menggunakan pencarian id user
         $sql="select * from users order by id_user";
-        //fungsi untuk menghubungkan ke database
         $tampil = mysqli_query($con,$sql);
         if (mysqli_num_rows($tampil) > 0) {
             $no=1;
             while ($r = mysqli_fetch_array($tampil)){
-                //menampilkan id user, nama dan email
-                //fungsi hapus data menggunakan pencarian id user
                 echo "<tr><td>$no</td><td>$r[id_user]</td>
                 <td>$r[nama]</td>
                 <td>$r[email]</td>
@@ -26,8 +21,7 @@
             echo "</table>";
         } else {
         echo "0 results";
-        }
-    //fungsi untuk menutup database   
+        }   
     mysqli_close($con);
 ?>
 <br><br>
